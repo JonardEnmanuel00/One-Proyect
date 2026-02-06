b68d588d2a1e8e6ebe0de48bb32bf88f8f5f3d96
Jonalo Enmanuel <153546028+JonardEnmanuel00@users.noreply.github.com>
2026-02-05 20:11:39 -0400

    Diseñando Mi formulario

--- One Exercise Prectice/One Exercise Prectice/Form1.Designer.cs
+++ One Exercise Prectice/One Exercise Prectice/Form1.Designer.cs
@@ -28,12 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            Etiqueta = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // Etiqueta
+            // 
+            Etiqueta.AutoSize = true;
+            Etiqueta.Location = new Point(28, 25);
+            Etiqueta.Name = "Etiqueta";
+            Etiqueta.Size = new Size(49, 20);
+            Etiqueta.TabIndex = 0;
+            Etiqueta.Text = "Name";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(28, 48);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(168, 46);
+            button1.Name = "button1";
+            button1.Size = new Size(248, 102);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.HotTrack;
             ClientSize = new Size(800, 450);
-            Text = "Form1";
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(Etiqueta);
+            Cursor = Cursors.IBeam;
+            ForeColor = SystemColors.ActiveCaptionText;
+            Name = "Form1";
+            Text = "One Exercise";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label Etiqueta;
+        private TextBox textBox1;
+        private Button button1;
     }
 }
