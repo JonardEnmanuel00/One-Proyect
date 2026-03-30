463e9eb09f37d1c3149230e45fbbfa5f5a2f1dcb
EnmanuelJonito <153546028+JonardEnmanuel00@users.noreply.github.com>
2026-03-30 18:20:40 -0400

    Versión final probada y funcional

---
+++ ConsultorioDental_Proyecto/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace ConsultorioDental_Proyecto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPaciente = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpFecha = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtHora = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtDentista = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cmbMotivo = new System.Windows.Forms.ComboBox();
+            this.dgvCitas = new System.Windows.Forms.DataGridView();
+            this.btnAgendar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnExportar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCitas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID de Cita:";
+            // 
+            // txtID
+            // 
+            this.txtID.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtID.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(16, 53);
+            this.txtID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(319, 32);
+            this.txtID.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 105);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre Paciente:";
+            // 
+            // txtPaciente
+            // 
+            this.txtPaciente.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtPaciente.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPaciente.Location = new System.Drawing.Point(17, 146);
+            this.txtPaciente.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPaciente.Name = "txtPaciente";
+            this.txtPaciente.Size = new System.Drawing.Size(320, 32);
+            this.txtPaciente.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 204);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Fecha:";
+            // 
+            // dtpFecha
+            // 
+            this.dtpFecha.CalendarForeColor = System.Drawing.SystemColors.ControlDark;
+            this.dtpFecha.CalendarMonthBackground = System.Drawing.SystemColors.Info;
+            this.dtpFecha.CalendarTitleBackColor = System.Drawing.SystemColors.Info;
+            this.dtpFecha.CalendarTitleForeColor = System.Drawing.SystemColors.Info;
+            this.dtpFecha.CalendarTrailingForeColor = System.Drawing.SystemColors.Info;
+            this.dtpFecha.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpFecha.Location = new System.Drawing.Point(16, 254);
+            this.dtpFecha.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dtpFecha.Name = "dtpFecha";
+            this.dtpFecha.Size = new System.Drawing.Size(400, 32);
+            this.dtpFecha.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 319);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Hora (HH:mm):";
+            // 
+            // txtHora
+            // 
+            this.txtHora.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtHora.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHora.Location = new System.Drawing.Point(16, 363);
+            this.txtHora.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtHora.Name = "txtHora";
+            this.txtHora.Size = new System.Drawing.Size(321, 32);
+            this.txtHora.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(17, 426);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 23);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Dentista:";
+            // 
+            // txtDentista
+            // 
+            this.txtDentista.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.txtDentista.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDentista.Location = new System.Drawing.Point(16, 466);
+            this.txtDentista.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtDentista.Name = "txtDentista";
+            this.txtDentista.Size = new System.Drawing.Size(319, 32);
+            this.txtDentista.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(17, 545);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 23);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Motivo:";
+            // 
+            // cmbMotivo
+            // 
+            this.cmbMotivo.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.cmbMotivo.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbMotivo.FormattingEnabled = true;
+            this.cmbMotivo.Items.AddRange(new object[] {
+            "Limpieza ",
+            "Extracción ",
+            "Revisión"});
+            this.cmbMotivo.Location = new System.Drawing.Point(16, 592);
+            this.cmbMotivo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.cmbMotivo.Name = "cmbMotivo";
+            this.cmbMotivo.Size = new System.Drawing.Size(319, 31);
+            this.cmbMotivo.TabIndex = 11;
+            // 
+            // dgvCitas
+            // 
+            this.dgvCitas.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvCitas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCitas.Location = new System.Drawing.Point(505, 15);
+            this.dgvCitas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dgvCitas.Name = "dgvCitas";
+            this.dgvCitas.RowHeadersWidth = 51;
+            this.dgvCitas.Size = new System.Drawing.Size(1293, 382);
+            this.dgvCitas.TabIndex = 12;
+            this.dgvCitas.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvCitas_CellClick);
+            // 
+            // btnAgendar
+            // 
+            this.btnAgendar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnAgendar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAgendar.Location = new System.Drawing.Point(505, 414);
+            this.btnAgendar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAgendar.Name = "btnAgendar";
+            this.btnAgendar.Size = new System.Drawing.Size(557, 46);
+            this.btnAgendar.TabIndex = 14;
+            this.btnAgendar.Text = "Agendar Cita";
+            this.btnAgendar.UseVisualStyleBackColor = false;
+            this.btnAgendar.Click += new System.EventHandler(this.btnAgendar_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnActualizar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnActualizar.Location = new System.Drawing.Point(1241, 414);
+            this.btnActualizar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(557, 46);
+            this.btnActualizar.TabIndex = 15;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = false;
+            this.btnActualizar.Click += new System.EventHandler(this.btnActualizar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnEliminar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminar.Location = new System.Drawing.Point(1241, 501);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(557, 46);
+            this.btnEliminar.TabIndex = 16;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnExportar
+            // 
+            this.btnExportar.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnExportar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExportar.Location = new System.Drawing.Point(505, 501);
+            this.btnExportar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnExportar.Name = "btnExportar";
+            this.btnExportar.Size = new System.Drawing.Size(557, 46);
+            this.btnExportar.TabIndex = 17;
+            this.btnExportar.Text = "Exportar CSV";
+            this.btnExportar.UseVisualStyleBackColor = false;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnSalir.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(505, 592);
+            this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(1293, 46);
+            this.btnSalir.TabIndex = 18;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightCyan;
+            this.ClientSize = new System.Drawing.Size(1811, 690);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnExportar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnAgendar);
+            this.Controls.Add(this.dgvCitas);
+            this.Controls.Add(this.cmbMotivo);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDentista);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtHora);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dtpFecha);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPaciente);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistema de Citas - Consultorio Dental";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCitas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPaciente;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker dtpFecha;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtHora;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtDentista;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cmbMotivo;
+        private System.Windows.Forms.DataGridView dgvCitas;
+        private System.Windows.Forms.Button btnAgendar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnExportar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ ConsultorioDental_Proyecto/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConsultorioDental_Proyecto
+{
+    public partial class Form1 : Form
+    {
+
+        
+        System.Collections.Generic.List<Cita> listaCitas = new System.Collections.Generic.List<Cita>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAgendar_Click(object sender, EventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(txtID.Text) ||
+                string.IsNullOrWhiteSpace(txtPaciente.Text) ||
+                string.IsNullOrWhiteSpace(txtHora.Text) ||
+                cmbMotivo.SelectedIndex == -1)
+            {
+                MessageBox.Show("Todos los campos con * son obligatorios.");
+                return;
+            }
+
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtID.Text) || string.IsNullOrWhiteSpace(txtPaciente.Text))
+                {
+                    System.Windows.Forms.MessageBox.Show("Por favor, llena el ID y el Nombre.");
+                    return;
+                }
+
+                if (listaCitas.Exists(x => x.ID == txtID.Text))
+                {
+                    System.Windows.Forms.MessageBox.Show("Este ID ya existe. Usa uno diferente.");
+                    return;
+                }
+
+                Cita nuevaCita = new Cita
+                {
+                    ID = txtID.Text,
+                    Paciente = txtPaciente.Text,
+                    Fecha = dtpFecha.Value,
+                    Hora = txtHora.Text,
+                    Dentista = txtDentista.Text,
+                    Motivo = cmbMotivo.Text
+                };
+
+                listaCitas.Add(nuevaCita);
+                dgvCitas.DataSource = null; // Truco para refrescar
+                dgvCitas.DataSource = listaCitas;
+
+                System.Windows.Forms.MessageBox.Show("Cita agendada con éxito.");
+
+                txtID.Clear();
+                txtPaciente.Clear();
+            }
+            catch (System.Exception ex)
+            {
+                System.Windows.Forms.MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrWhiteSpace(txtID.Text))
+                {
+                    MessageBox.Show("Escribe el ID de la cita que deseas eliminar.");
+                    return;
+                }
+
+                Cita citaAEliminar = listaCitas.Find(x => x.ID == txtID.Text);
+
+                if (citaAEliminar != null)
+                {
+                    // --- ESTO ES LO NUEVO: Una pregunta de seguridad ---
+                    DialogResult resultado = MessageBox.Show($"¿Estás seguro de que deseas eliminar la cita de {citaAEliminar.Paciente}?",
+                        "Confirmar Eliminación", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+                    if (resultado == DialogResult.Yes)
+                    {
+                        listaCitas.Remove(citaAEliminar);
+
+                        // Refrescar el Grid
+                        dgvCitas.DataSource = null;
+                        dgvCitas.DataSource = listaCitas;
+
+                        MessageBox.Show("Cita eliminada correctamente.");
+
+                        // Limpiamos los campos para que queden vacíos
+                        txtID.Clear();
+                        txtPaciente.Clear();
+                        txtHora.Clear();
+                        txtDentista.Clear();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("No se encontró ninguna cita con ese ID.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar: " + ex.Message);
+            }
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Cita citaAEditar = listaCitas.Find(x => x.ID == txtID.Text);
+
+                if (citaAEditar != null)
+                {
+                    citaAEditar.Paciente = txtPaciente.Text;
+                    citaAEditar.Fecha = dtpFecha.Value;
+                    citaAEditar.Hora = txtHora.Text;
+                    citaAEditar.Dentista = txtDentista.Text;
+                    citaAEditar.Motivo = cmbMotivo.Text;
+
+                    dgvCitas.DataSource = null;
+                    dgvCitas.DataSource = listaCitas;
+
+                    MessageBox.Show("Cita actualizada con éxito.");
+                }
+                else
+                {
+                    MessageBox.Show("No se puede actualizar: El ID no existe.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocurrió un error: " + ex.Message);
+            }
+        }
+
+        private void dgvCitas_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                try
+                {
+                    DataGridViewRow fila = dgvCitas.Rows[e.RowIndex];
+
+                    // Asignamos los valores de la fila a los cuadros de texto
+                    txtID.Text = fila.Cells["ID"].Value.ToString();
+                    txtPaciente.Text = fila.Cells["Paciente"].Value.ToString();
+                    txtHora.Text = fila.Cells["Hora"].Value.ToString();
+                    txtDentista.Text = fila.Cells["Dentista"].Value.ToString();
+                    cmbMotivo.Text = fila.Cells["Motivo"].Value.ToString();
+                    dtpFecha.Value = Convert.ToDateTime(fila.Cells["Fecha"].Value);
+                }
+                catch { /* Si falta algún dato, que no explote */ }
+            }
+        }
+    }
+}
